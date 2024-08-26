c9f0769c430d0487bda46e1f5dea14ca15813721
AL2002MI08 <alexandramizero@gmail.com>
2024-08-26 15:55:22 +0200

    chore: create folder structure and browser router configuration

--- src/index.tsx
+++ src/index.tsx
@@ -3,11 +3,18 @@ import './index.css'
 import App from './App'
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+  RouterProvider,
+} from 'react-router-dom'
 
 // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
 const root = ReactDOM.createRoot(document.getElementById('root')!)
+const router = createBrowserRouter(createRoutesFromElements(<Route path="/" element={<App />} />))
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
