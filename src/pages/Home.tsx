ae3a5bbb3d054801ba95f3fb240903a9f5f9eabb
manzitresor <manzitre@gmail.com>
2024-08-26 20:00:06 +0200

    feat: add home page

---
+++ src/pages/Home.tsx
@@ -0,0 +1,15 @@
+export default function Home() {
+  return (
+    <>
+      <div className="h-screen w-full bg-primary-indigo py-10">
+        <h1 className="flex flex-col text-center font-lora text-5xl font-bold text-gray-50">
+          MONI <span className="font-medium italic">share</span>
+        </h1>
+        <p className="mx-auto my-12 flex flex-col text-center font-lora text-xl font-medium text-gray-100">
+          Hello Manuela!!
+          <span>What are you up to today?</span>
+        </p>
+      </div>
+    </>
+  )
+}
