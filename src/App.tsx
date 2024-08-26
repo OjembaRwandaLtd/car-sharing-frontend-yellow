15b2a519bdd39f1eb51c4c36e31bc4c318e38756
AL2002MI08 <alexandramizero@gmail.com>
2024-08-26 15:12:21 +0200

    chore: add custom fonts to the config file

--- src/App.tsx
+++ src/App.tsx
@@ -8,12 +8,13 @@ configure({
     autoCancel: false,
   },
 })
-
 function App(): ReactElement {
   return (
     <main className="mx-auto flex min-h-screen w-1/3 flex-col gap-8 py-10">
-      <h1 className="text-4xl font-bold">Hello to MoniShare</h1>
-      <p>If you can read this, you have successfully started the base frontend repository!</p>
+      <h1 className="text-4xl font-inter">Hello to MoniShare</h1>
+      <p className="font-lora">
+        If you can read this, you have successfully started the base frontend repository!
+      </p>
       <p>Happy coding!</p>
     </main>
   )
