d063872066299d7d5e29c2bd70d3d9f66a1366e2
AL2002MI08 <alexandramizero@gmail.com>
2024-08-27 16:02:18 +0200

    chore: add new changes for button feature

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import { ReactElement } from 'react'
 import { configure } from 'axios-hooks'
-import Home from './pages/Home'
+
 // Configure axios hooks
 // Do not delete this if you want to use the provided API hooks in `src/hooks`
 configure({
@@ -10,9 +10,13 @@ configure({
 })
 function App(): ReactElement {
   return (
-    <>
-      <Home />
-    </>
+    <main className="mx-auto flex min-h-screen w-1/3 flex-col gap-8 py-10">
+      <h1 className="font-inter text-4xl font-bold text-primary-blue">Hello to MoniShare</h1>
+      <p className="font-lora">
+        If you can read this, you have successfully started the base frontend repository!
+      </p>
+      <p>Happy coding!</p>
+    </main>
   )
 }
 
--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,20 +1,14 @@
 interface Props {
   buttonText: string
-  background?: string
-  textColor: string
-  border: {
-    color?: string
-    radius?: string
-    width?: string
-  }
+  customStyles: string
 }
 
-export default function Button({ buttonText, background, textColor, border }: Props) {
+export default function Button({ buttonText, customStyles }: Props) {
   return (
     <>
       <button
-        className={`bg- mx-4 h-11 w-11/12 rounded-3xl text-center font-inter text-sm font-bold ${background} ${textColor} border-${border.width} border-${border.radius}
-        ${border.color} `}
+        className={`bg- mx-4 h-11 w-11/12 rounded-3xl text-center font-inter text-sm font-bold 
+        ${customStyles} `}
       >
         {buttonText}
       </button>
