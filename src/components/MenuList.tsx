51da9bdf9488194dadebf14a81d1b15d4830b808
Victor Mugisha Shyaka <victormugishavm6@gmail.com>
2024-08-28 14:44:18 +0200

    feat: add path to the menu list items

--- src/components/MenuList.tsx
+++ src/components/MenuList.tsx
@@ -11,13 +11,13 @@ import { useState } from 'react'
 export default function MenuList() {
   const [menuText, setMenuText] = useState<string>('Menu')
   const generalDetails = [
-    { icon: <CarIcon />, title: 'Book A Car', path: '' },
-    { icon: <BookingsIcon />, title: 'Bookings', path: '' },
+    { icon: <CarIcon />, title: 'Book A Car', path: 'new-booking' },
+    { icon: <BookingsIcon />, title: 'Bookings', path: 'my-bookings' },
   ]
   const myCarsDetails = [
-    { icon: <CarsIcon />, title: 'See my Cars', path: '' },
-    { icon: <ListIcon />, title: "My Car's Bookings", path: '' },
-    { icon: <CarPlusIcon />, title: 'Add New Car', path: '' },
+    { icon: <CarsIcon />, title: 'See my Cars', path: 'my-cars' },
+    { icon: <ListIcon />, title: "My Car's Bookings", path: 'manage-bookings' },
+    { icon: <CarPlusIcon />, title: 'Add New Car', path: 'add-car' },
   ]
   const generalItems = generalDetails.map(generalDetail => (
     <HUMenuItem key={generalDetail.title}>
@@ -39,17 +39,15 @@ export default function MenuList() {
       <MenuButton className=" bg-primary-blue text-white" onClick={handleMenuClick}>
         {menuText}
       </MenuButton>
-      <MenuItems>
-        <div className="w-56 rounded-lg bg-secondary-indigo px-6 pb-6">
-          <div className="flex flex-col gap-4 pt-4">{generalItems}</div>
-          <div className="my-4 flex flex-col gap-4 border-y py-4">
-            <h3 className="font-semibold text-white">My cars</h3>
-            {myCarsItems}
-          </div>
-          <HUMenuItem>
-            <MenuItem icon={<LogoutIcon />} path="/login" title="Log Out" />
-          </HUMenuItem>
+      <MenuItems className="w-56 rounded-lg bg-secondary-indigo px-6 pb-6">
+        <div className="flex flex-col gap-4 pt-4">{generalItems}</div>
+        <div className="my-4 flex flex-col gap-4 border-y py-4">
+          <h3 className="font-semibold text-white">My cars</h3>
+          {myCarsItems}
         </div>
+        <HUMenuItem>
+          <MenuItem icon={<LogoutIcon />} path="login" title="Log Out" />
+        </HUMenuItem>
       </MenuItems>
     </Menu>
   )
