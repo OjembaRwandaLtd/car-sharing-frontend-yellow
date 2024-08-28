7ff7d312249ec86a90cd2bf39e5518e5d3384374
manzitresor <manzitre@gmail.com>
2024-08-28 11:07:26 +0200

    chore: add changes requested to make button more flexible

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,16 +1,21 @@
-interface Props {
-  buttonText: string
-  customStyles: string
+export enum ButtonStyles {
+  Primary = 'bg-white text-primary-indigo',
+  Secondary = 'border-2 border-white text-white',
 }
 
-export default function Button({ buttonText, customStyles }: Props) {
+interface ButtonProps {
+  variantName: ButtonStyles
+  children: React.ReactNode
+}
+
+export default function Button({ children, variantName }: ButtonProps) {
   return (
     <>
       <button
-        className={`bg- mx-4 h-11 w-11/12 rounded-3xl text-center font-inter text-sm font-bold 
-        ${customStyles} `}
+        className={`mx-4 h-11 w-11/12 rounded-3xl text-center font-inter text-sm font-bold 
+        ${variantName} `}
       >
-        {buttonText}
+        {children}
       </button>
     </>
   )
