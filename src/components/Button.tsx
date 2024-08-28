aec71bc1730eb934a751fc058fb4c476fcd6f63e
AL2002MI08 <alexandramizero@gmail.com>
2024-08-28 15:05:11 +0200

    chore: add handleClick and disable props

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,20 +1,24 @@
-export enum ButtonStyles {
-  Primary = 'bg-white text-primary-indigo',
-  Secondary = 'border-2 border-white text-white',
+import classNames from 'classnames'
+export const ButtonStyles = {
+  primary: 'bg-white text-primary-indigo',
+  secondary: 'border-2 border-white text-white',
 }
 
 interface ButtonProps {
-  ButtonStyles: ButtonStyles
+  variant: keyof typeof ButtonStyles
   children: React.ReactNode
+  handleClick: () => void
+  disabled: boolean
 }
 
-export default function Button({ children, ButtonStyles }: ButtonProps) {
+export default function Button({ children, variant, handleClick, disabled }: ButtonProps) {
+  const buttonClass = classNames(
+    'mx-3 rounded-3xl py-3 text-center font-inter text-sm font-bold',
+    ButtonStyles[variant],
+  )
   return (
     <>
-      <button
-        className={`mx-3 rounded-3xl py-3 text-center font-inter text-sm font-bold 
-        ${ButtonStyles} `}
-      >
+      <button className={buttonClass} onClick={handleClick} aria-disabled={disabled}>
         {children}
       </button>
     </>
