40c46f241a5e15eee5ebc3d81ea4244d85478d3f
l-isaro <l.isaro@alustudent.com>
2024-08-29 16:24:05 +0200

    fix: fix main background and LoginLayout naming

--- src/layouts/GuestLayout.tsx
+++ src/layouts/GuestLayout.tsx
@@ -1,11 +1,11 @@
 import GuestNavbar from '../components/GuestNavbar'
 import { Outlet } from 'react-router-dom'
 
-export default function LoginLayout() {
+export default function GuestLayout() {
   return (
-    <>
+    <div className="min-h-screen bg-primary-indigo">
       <GuestNavbar />
       <Outlet />
-    </>
+    </div>
   )
 }
