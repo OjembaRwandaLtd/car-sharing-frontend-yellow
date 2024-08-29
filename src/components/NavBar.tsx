14cec6eec3278439847e134ba5af3119150f8298
InshutiSouvede <isouvede2@gmail.com>
2024-08-29 13:05:46 +0200

    feat: adjust logo in navbar

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -6,11 +6,9 @@ export default function NavBar() {
   return (
     <header className="relative z-20 flex justify-between rounded-b-lg bg-primary-blue p-5 font-inter text-gray-100 shadow-lg">
       <MenuList />
-      <div className="absolute left-1/2 -translate-x-1/2 rounded-full bg-primary-blue px-2 pb-3 shadow-lg">
-        <button>
-          <Logo />
-        </button>
-      </div>
+      <button className="absolute left-1/2 -translate-x-1/2 rounded-b-full bg-primary-blue px-4 pb-3 shadow-lg">
+        <Logo className="pb-2" />
+      </button>
       <button>
         <ProfileIcon />
       </button>
