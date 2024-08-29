54cc69d1d1ca57412e7768c959f8f35d6716b552
AL2002MI08 <alexandramizero@gmail.com>
2024-08-29 15:18:13 +0200

    feat: implement error page

---
+++ src/pages/ErrorPage.tsx
@@ -0,0 +1,22 @@
+import Button from '../components/Button'
+import ErrorPageIcon from '../assets/ErrorPageIcon'
+
+export default function ErrorPage() {
+  return (
+    <div className="h-screen w-full bg-primary-indigo py-10 px-4">
+      <h1 className="flex flex-col text-center font-lora text-5xl font-bold text-gray-50">
+        OOOOOPS!
+      </h1>
+      <div className="mt-10 flex justify-center">
+        <ErrorPageIcon />
+      </div>
+      <p className="mx-auto my-12 flex flex-col text-center font-lora text-xl font-medium text-gray-100">
+        Something went wrong.
+        <span>We will solve your issue soon.</span>
+      </p>
+      <Button variant="secondary" disabled={false} handleClick={() => {}}>
+        Go back
+      </Button>
+    </div>
+  )
+}
