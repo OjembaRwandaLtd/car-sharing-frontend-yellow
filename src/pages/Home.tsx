2b65d52df8353282f980ccedc05103e9dc308638
manzitresor <manzitre@gmail.com>
2024-08-29 12:55:09 +0200

    chore: add button component to home page and create some dummy data

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,4 +1,22 @@
+import Button from '../components/Button'
+
 export default function Home() {
+  const users = [{ username: 'Manuela' }, { username: 'Alexa' }]
+  const buttonItems = [
+    {
+      name: 'See My Cars',
+      variant: 'primary' as const,
+      disabled: false,
+      handleClick: () => {},
+    },
+    {
+      name: 'See My Bookings',
+      variant: 'secondary' as const,
+      disabled: false,
+      handleClick: () => {},
+    },
+  ]
+
   return (
     <>
       <div className="h-screen w-full bg-primary-indigo py-10">
@@ -6,9 +24,25 @@ export default function Home() {
           MONI <span className="font-medium italic">share</span>
         </h1>
         <p className="mx-auto my-12 flex flex-col text-center font-lora text-xl font-medium text-gray-100">
-          Hello Manuela!!
+          Hello {users[0].username}!!
           <span>What are you up to today?</span>
         </p>
+        <Button variant="primary" disabled={false} handleClick={() => {}}>
+          Book A car
+        </Button>
+        <p className="my-7 text-center font-lora text-xl font-medium text-gray-100">or</p>
+        <div className="flex flex-col gap-4">
+          {buttonItems.map((buttonItem, index) => (
+            <Button
+              key={index}
+              variant={buttonItem.variant}
+              disabled={buttonItem.disabled}
+              handleClick={buttonItem.handleClick}
+            >
+              {buttonItem.name}
+            </Button>
+          ))}
+        </div>
       </div>
     </>
   )
