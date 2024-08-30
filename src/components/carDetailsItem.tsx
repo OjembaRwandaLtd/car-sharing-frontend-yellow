cce7341142f6d4ecdcf9ace299e0158aca714f68
AL2002MI08 <alexandramizero@gmail.com>
2024-08-30 14:00:55 +0200

    feat: create car details item component

---
+++ src/components/carDetailsItem.tsx
@@ -0,0 +1,13 @@
+interface carDetailItemProps {
+  icon: React.ReactNode
+  title: string
+}
+
+export default function carDetailsItem({ title, icon }: carDetailItemProps) {
+  return (
+    <div className="flex items-center gap-3 font-inter text-white">
+      <span className="h-6 w-6">{icon}</span>
+      <span>{title}</span>
+    </div>
+  )
+}
