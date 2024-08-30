9c7c047fe2a06186aa702c7cb6390e3bb7d066bf
AL2002MI08 <alexandramizero@gmail.com>
2024-08-30 14:46:46 +0200

    feat: use car detail item component on car details page

--- src/components/carDetailsItem.tsx
+++ src/components/carDetailsItem.tsx
@@ -1,9 +1,9 @@
 interface carDetailItemProps {
   icon: React.ReactNode
-  title: string
+  title?: string | number | null
 }
 
-export default function carDetailsItem({ title, icon }: carDetailItemProps) {
+export default function CarDetailsItem({ title, icon }: carDetailItemProps) {
   return (
     <div className="flex items-center gap-3 font-inter text-white">
       <span className="h-6 w-6">{icon}</span>
--- src/pages/CarDetails.tsx
+++ src/pages/CarDetails.tsx
@@ -1,8 +1,21 @@
 import { ChevronBackIcon } from '../assets/ChevronBackIcon'
+import FuelIcon from '../assets/FuelIcon'
+import CarDetailsItem from '../components/carDetailsItem'
 import useCar from '../hooks/useCar'
 
 export default function CarDetails() {
   const [{ data }] = useCar(1)
+  const carDetails = [
+    { title: data?.ownerId, icon: <FuelIcon /> },
+    { title: data?.carTypeId, icon: <FuelIcon /> },
+    { title: data?.licensePlate, icon: <FuelIcon /> },
+    { title: data?.horsepower, icon: <FuelIcon /> },
+    { title: data?.info, icon: <FuelIcon /> },
+    { title: data?.fuelType, icon: <FuelIcon /> },
+  ]
+  const allCarDetails = carDetails
+    .filter(item => item.title)
+    .map((item, id) => <CarDetailsItem key={id} title={item.title} icon={item.icon} />)
   return (
     <div className="px-5 py-8 text-gray-100">
       <div className="flex items-center">
@@ -10,6 +23,7 @@ export default function CarDetails() {
         <h1 className="w-96 text-center font-lora text-3xl font-semibold text-white">DETAILS</h1>
       </div>
       <h2>{data?.name}</h2>
+      <div className="flex flex-col">{allCarDetails}</div>
     </div>
   )
 }
