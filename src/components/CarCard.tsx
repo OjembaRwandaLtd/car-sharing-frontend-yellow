6b31361d6a5d7c3b1b48b5a816fedcd03e7a7018
manzitresor <manzitre@gmail.com>
2024-08-30 12:39:13 +0200

    chore: add car image based on cartype

--- src/components/CarCard.tsx
+++ src/components/CarCard.tsx
@@ -1,9 +1,24 @@
-import { CarDto } from '../util/api'
+import ProfileIcon from '../assets/ProfileIcon'
+import { CarDto, CarTypeDto } from '../util/api'
+import { useUser } from '../hooks'
 
 interface CarCardProps {
   car: CarDto
+  carType: CarTypeDto | undefined
 }
 
-export default function CarCard({ car }: Readonly<CarCardProps>) {
-  return <div>{car.id}</div>
+export default function CarCard({ car, carType }: Readonly<CarCardProps>) {
+  const [{ data: user }] = useUser(car.ownerId)
+  console.log(user?.name)
+  return (
+    <div className="rounded-lg bg-secondary-indigo">
+      <img src={carType?.imageUrl} alt={carType?.name} />
+      <div>
+        <h3>{car.name}</h3>
+        <div>
+          <ProfileIcon /> <span>Manuela</span>
+        </div>
+      </div>
+    </div>
+  )
 }
--- src/pages/NewBooking.tsx
+++ src/pages/NewBooking.tsx
@@ -1,7 +1,16 @@
 import CarCard from '../components/CarCard'
 import useCars from '../hooks/useCars'
+import { useCarTypes } from '../hooks'
 
 export default function NewBooking() {
+  const [{ data: carTypes }] = useCarTypes()
   const [{ data }] = useCars()
-  return <>{data?.map(car => <CarCard key={car.id} car={car} />)}</>
+  return (
+    <>
+      {data?.map(car => {
+        const carType = carTypes?.find(type => type.id === car.carTypeId)
+        return <CarCard key={car.id} car={car} carType={carType} />
+      })}
+    </>
+  )
 }
