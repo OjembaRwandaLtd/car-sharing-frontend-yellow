0b81fd2fd3bf85823d00133cade2fac2c7124c42
manzitresor <manzitre@gmail.com>
2024-09-02 13:59:54 +0200

    fix: add navbar flexible and render items dynamically

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,21 +1,33 @@
+import classNames from 'classnames'
 import Logo from '../assets/Logo'
-import ProfileIcon from '../assets/ProfileIcon'
-import MenuList from './MenuList'
 import { NavLink } from 'react-router-dom'
 
-export default function NavBar() {
+interface NavBarProps {
+  className?: string
+  MenuList?: React.ReactNode
+  ProfileIcon?: React.ReactNode
+}
+
+export default function NavBar({ className = '', MenuList, ProfileIcon }: Readonly<NavBarProps>) {
+  const hasIcons = MenuList || ProfileIcon
+
+  const navClassname = classNames(
+    'sticky top-0 z-20 flex justify-between rounded-b-lg bg-primary-blue p-5 font-inter text-gray-100 shadow-lg',
+    {
+      [className]: !hasIcons,
+    },
+  )
+
   return (
-    <header className="sticky top-0 z-20 flex justify-between rounded-b-lg bg-primary-blue p-5 font-inter text-gray-100 shadow-lg">
-      <MenuList />
+    <header className={navClassname}>
+      {MenuList}
       <NavLink
-        to="/"
+        to={hasIcons ? '/' : ''}
         className="absolute left-1/2 -translate-x-1/2 rounded-b-full bg-primary-blue px-4 pb-3 shadow-lg"
       >
         <Logo className="pb-2" />
       </NavLink>
-      <NavLink to="/">
-        <ProfileIcon />
-      </NavLink>
+      {ProfileIcon && <NavLink to="/">{ProfileIcon}</NavLink>}
     </header>
   )
 }
