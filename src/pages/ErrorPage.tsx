0621e28d843f8559b28943e871912a7fc55bed27
InshutiSouvede <isouvede2@gmail.com>
2024-09-02 11:24:31 +0200

    fix: add custom white color

--- src/pages/ErrorPage.tsx
+++ src/pages/ErrorPage.tsx
@@ -4,13 +4,13 @@ import ErrorPageIcon from '../assets/ErrorPageIcon'
 export default function ErrorPage() {
   return (
     <div className="h-screen w-full px-4 py-10">
-      <h1 className="flex flex-col text-center font-lora text-5xl font-bold text-gray-50">
+      <h1 className="flex flex-col text-center font-lora text-5xl font-bold bg-primary-white">
         OOOOOPS!
       </h1>
       <div className="mt-10 flex justify-center">
         <ErrorPageIcon />
       </div>
-      <p className="mx-auto my-12 flex flex-col text-center font-lora text-xl font-medium text-gray-100">
+      <p className="mx-auto my-12 flex flex-col text-center font-lora text-xl font-medium bg-primary-white">
         Something went wrong.
         <span>We will solve your issue soon.</span>
       </p>
