d4970c97ce8627a477e3f610c6c13670042bfbdb
AL2002MI08 <alexandramizero@gmail.com>
2024-09-02 15:48:53 +0200

    feat: add car type and image

--- src/pages/CarDetails.tsx
+++ src/pages/CarDetails.tsx
@@ -1,28 +1,38 @@
 import { ChevronBackIcon } from '../assets/ChevronBackIcon'
 import FuelIcon from '../assets/FuelIcon'
 import CarDetailsItem from '../components/carDetailsItem'
+import { useCarTypes } from '../hooks'
 import useCar from '../hooks/useCar'
 
 export default function CarDetails() {
-  const [{ data }] = useCar(1)
+  const [{ data: carTypes }] = useCarTypes()
+  const [{ data: carData }] = useCar(2)
+
+  const currentCarTypes = carTypes?.find(carType => carType.id === carData?.carTypeId)
+  const carImage = carTypes?.find(item => item.id === currentCarTypes?.id)?.imageUrl
+  const carTypeName = currentCarTypes?.name.split(' ').slice(-1).join(' ')
+
   const carDetails = [
-    { title: data?.ownerId, icon: <FuelIcon /> },
-    { title: data?.carTypeId, icon: <FuelIcon /> },
-    { title: data?.licensePlate, icon: <FuelIcon /> },
-    { title: data?.horsepower, icon: <FuelIcon /> },
-    { title: data?.info, icon: <FuelIcon /> },
-    { title: data?.fuelType, icon: <FuelIcon /> },
+    { title: carData?.ownerId, icon: <FuelIcon /> },
+    { title: carTypeName, icon: <FuelIcon /> },
+    { title: carData?.licensePlate, icon: <FuelIcon /> },
+    { title: carData?.horsepower, icon: <FuelIcon /> },
+    { title: carData?.fuelType, icon: <FuelIcon /> },
+    { title: carData?.info, icon: <FuelIcon /> },
   ]
+
   const allCarDetails = carDetails
     .filter(item => item.title)
     .map((item, id) => <CarDetailsItem key={id} title={item.title} icon={item.icon} />)
+
   return (
     <div className="px-5 py-8 text-gray-100">
       <div className="flex items-center">
         <ChevronBackIcon />
         <h1 className="w-96 text-center font-lora text-3xl font-semibold text-white">DETAILS</h1>
       </div>
-      <h2>{data?.name}</h2>
+      <img src={carImage} alt="car" />
+      <h2>{carData?.name}</h2>
       <div className="flex flex-col">{allCarDetails}</div>
     </div>
   )
