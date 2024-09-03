febc4c420f72fc15fc8a9d7a809ad137e953d6b6
InshutiSouvede <isouvede2@gmail.com>
2024-09-03 12:32:12 +0200

    feat: implement car details card

---
+++ src/components/CarDetailsCard.tsx
@@ -0,0 +1,39 @@
+import CarIcon from '../assets/CarIcon'
+import DangerIcon from '../assets/DangerIcon'
+import FuelIcon from '../assets/FuelIcon'
+import HorseIcon from '../assets/HorseIcon'
+import LicensePlateIcon from '../assets/LicensePlateIcon'
+import ProfileIcon from '../assets/ProfileIcon'
+import { useUser } from '../hooks'
+import { CarDto } from '../util/api'
+
+interface carDetailItemProps {
+  carData?: CarDto
+  carType?: string
+}
+
+export default function CarDetailsCard({ carType, carData }: carDetailItemProps) {
+  const ownerId = carData?.ownerId || ''
+  const [{ data: userData }] = useUser(ownerId)
+  const carDetails = [
+    { title: userData?.name, icon: <ProfileIcon /> },
+    { title: carType, icon: <CarIcon /> },
+    { title: carData?.licensePlate, icon: <LicensePlateIcon /> },
+    { title: carData?.horsepower && `${carData?.horsepower}hp`, icon: <HorseIcon /> },
+    { title: carData?.fuelType, icon: <FuelIcon /> },
+    { title: carData?.info, icon: <DangerIcon />, className: 'font-bold' },
+  ]
+
+  const allCarDetails = carDetails
+    .filter(item => item.title)
+    .map(item => (
+      <div
+        key={item.title}
+        className="flex items-center gap-3 font-inter capitalize text-primary-white"
+      >
+        <span className="h-6 w-6">{item.icon}</span>
+        <span className={item.className}>{item.title}</span>
+      </div>
+    ))
+  return <div className="flex flex-col gap-2 px-11 pt-8">{allCarDetails}</div>
+}
