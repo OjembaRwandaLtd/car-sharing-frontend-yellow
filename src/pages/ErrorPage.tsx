f0698822b45d22021e11db90e3fbb01adf455dc3
l-isaro <l.isaro@alustudent.com>
2024-09-05 19:39:53 +0200

    fix: error page styles for desktop screen

--- src/pages/ErrorPage.tsx
+++ src/pages/ErrorPage.tsx
@@ -23,19 +23,19 @@ export default function ErrorPage() {
   })()
 
   return (
-    <div className="bg-primary-indigo text-white">
+    <div className="bg-primary-indigo text-white h-screen">
       <NavBar MenuList={<MenuList />} ProfileIcon={<ProfileIcon />} />
 
-      <div className="flex h-screen w-full flex-col px-4 py-10">
+      <div className="flex w-full flex-col px-4 py-10 md:h-min md:py-5 ">
         <h1 className="flex flex-col text-center font-lora text-5xl font-bold text-primary-white">
           OOOOOPS!
         </h1>
 
-        <div className="mt-10 flex justify-center">
+        <div className="mt-10 flex justify-center md:mt-5">
           <ErrorPageIcon />
         </div>
 
-        <p className="mx-auto my-12 flex flex-col text-center font-lora text-xl font-medium text-primary-white">
+        <p className="mx-auto my-12 flex flex-col text-center font-lora text-xl font-medium text-primary-white md:my-6">
           <span>{errorMessage}</span>
           We will solve your issue soon.
         </p>
