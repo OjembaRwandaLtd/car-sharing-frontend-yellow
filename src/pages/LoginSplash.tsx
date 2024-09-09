48b06685444d0516e1ae8c65131a82063c8ec6da
manzitresor <manzitre@gmail.com>
2024-09-09 14:28:51 +0200

    chore: add carsharing logo

--- src/pages/LoginSplash.tsx
+++ src/pages/LoginSplash.tsx
@@ -1,3 +1,9 @@
 export default function LoginSplash() {
-  return <div>Login Splash</div>
+  return (
+    <div className="w-full items-center px-4 py-10">
+      <h1 className="flex flex-col text-center font-lora text-5xl font-bold text-gray-50 md:flex-row md:justify-center md:gap-3 lg:text-7xl">
+        CAR <span className="font-medium italic">sharing</span>
+      </h1>
+    </div>
+  )
 }
