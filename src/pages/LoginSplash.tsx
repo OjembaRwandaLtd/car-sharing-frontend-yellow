e212931995f3da2bd59abc231162ce17a39db992
manzitresor <manzitre@gmail.com>
2024-09-09 14:30:34 +0200

    chore: add body content of the page

--- src/pages/LoginSplash.tsx
+++ src/pages/LoginSplash.tsx
@@ -4,6 +4,10 @@ export default function LoginSplash() {
       <h1 className="flex flex-col text-center font-lora text-5xl font-bold text-gray-50 md:flex-row md:justify-center md:gap-3 lg:text-7xl">
         CAR <span className="font-medium italic">sharing</span>
       </h1>
+      <p className="mx-auto my-32 flex flex-col text-center font-lora text-xl font-medium text-gray-100 lg:gap-4 lg:text-2xl">
+        Start sharing your car
+        <span>with the world</span>
+      </p>
     </div>
   )
 }
