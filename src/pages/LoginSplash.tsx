7c3eb9946783e723a091ca111832d1bf41cd3b42
manzitresor <manzitre@gmail.com>
2024-09-09 14:56:48 +0200

    chore: add Button component to allow navigation on login form

--- src/pages/LoginSplash.tsx
+++ src/pages/LoginSplash.tsx
@@ -1,13 +1,20 @@
+import Button, { ButtonBehavior } from '../components/Button'
+import { ButtonStyles } from '../components/Button'
+import { Links } from '../routes/router'
+
 export default function LoginSplash() {
   return (
-    <div className="w-full items-center px-4 py-10">
+    <div className="flex w-full flex-col items-center px-4 py-10">
       <h1 className="flex flex-col text-center font-lora text-5xl font-bold text-gray-50 md:flex-row md:justify-center md:gap-3 lg:text-7xl">
         CAR <span className="font-medium italic">sharing</span>
       </h1>
-      <p className="mx-auto my-32 flex flex-col text-center font-lora text-xl font-medium text-gray-100 lg:gap-4 lg:text-2xl">
+      <p className="mx-auto my-28 flex flex-col text-center font-lora text-xl font-medium text-gray-100 lg:gap-4 lg:text-2xl">
         Start sharing your car
         <span>with the world</span>
       </p>
+      <Button behavior={ButtonBehavior.Link} customStyles={ButtonStyles.primary} path={Links.LOGIN}>
+        Login In
+      </Button>
     </div>
   )
 }
