cd6107917311e4c051c71e32190fdac774d1d3f1
Victor Mugisha Shyaka <victormugishavm6@gmail.com>
2024-09-09 14:27:40 +0200

    chore: design the login form

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -6,10 +6,24 @@ export default function Login() {
       <h1 className="flex flex-col text-center font-lora text-5xl font-bold text-gray-50 md:flex-row md:justify-center md:gap-3 lg:text-7xl">
         CAR <span className="font-medium italic">sharing</span>
       </h1>
-      <div className="flex w-full flex-col items-center gap-8">
-        <p className="text-center font-lora font-medium text-white">Log in</p>
+      <div className="flex w-full flex-col items-center gap-8 pt-28">
+        <p className="text-center font-lora text-xl font-medium text-white">Log in</p>
 
         <form className="w-full">
+          <div className="mb-16 flex w-full flex-col gap-4">
+            <input
+              className="rounded-3xl border-none bg-secondary-indigo px-6 py-4 text-lg text-primary-white outline-none placeholder:text-primary-white"
+              type="text"
+              name="usernameOrEmail"
+              placeholder="Username / e-mail"
+            />
+            <input
+              className="rounded-3xl border-none bg-secondary-indigo px-6 py-4 text-lg text-primary-white outline-none placeholder:text-primary-white"
+              type="password"
+              name="password"
+              placeholder="Password"
+            />
+          </div>
           <Button behavior={ButtonBehavior.Button} customStyles={ButtonStyles.primary}>
             Log In
           </Button>
