cea3debca5ed07a1cf6b62ab430d92be3b2e9179
manzitresor <manzitre@gmail.com>
2024-09-11 10:34:08 +0200

    feat: add NewCar header

--- src/pages/cars/NewCar.tsx
+++ src/pages/cars/NewCar.tsx
@@ -1,3 +1,9 @@
 export default function NewCar() {
-  return <main>New Car</main>
+  return (
+    <main className="w-full items-center px-4 py-10">
+      <h1 className="text-center font-lora text-3xl font-medium uppercase text-primary-white">
+        new car
+      </h1>
+    </main>
+  )
 }
