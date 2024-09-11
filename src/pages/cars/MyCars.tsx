820a743ad6faaae75b6ad50870abee61305708ab
InshutiSouvede <isouvede2@gmail.com>
2024-09-11 16:07:32 +0200

    feat: implement delete functionality

--- src/pages/cars/MyCars.tsx
+++ src/pages/cars/MyCars.tsx
@@ -3,13 +3,39 @@ import Button, { ButtonBehavior, ButtonStyles } from '../../components/Button'
 import { Links } from '../../routes/router'
 import CarCard from '../../components/CarCard'
 import { useUser, useCars, useCarTypes } from '../../hooks'
+import { apiUrl } from '../../util/apiUrl'
+import { getAuthToken } from '../../util/auth'
+import { useEffect, useState } from 'react'
 
 export default function MyCars() {
-  const userId = 1
+  const [deleteId, setDeleteId] = useState(-1)
+  const userId = 11
   const [{ loading: carsLoading, error: carsError, data: cars }] = useCars()
   const [{ loading: carTypeLoading, error: carTypeError, data: carTypes }] = useCarTypes()
   const [{ loading: userLoading, error: userError, data: user }] = useUser(userId)
   const myCars = cars?.filter(car => car.ownerId === userId)
+
+  useEffect(() => {
+    if (deleteId === -1) return
+    const controller = new AbortController()
+    const signal = controller.signal
+    fetch(`${apiUrl}/cars/${deleteId}`, {
+      method: 'DELETE',
+      headers: {
+        Authorization: `Bearer ${getAuthToken()}`,
+      },
+      signal,
+    })
+      .then(response => response.json())
+      .then(() => alert('Successfully deleted car!'))
+      .catch(() => {
+        alert("Couldn't delete car")
+      })
+
+    return () => {
+      controller.abort()
+    }
+  }, [deleteId])
 
   if (carsLoading || carTypeLoading || userLoading) return <Spinner />
 
@@ -18,7 +44,7 @@ export default function MyCars() {
   if (!user) throw new Error('User not found')
   if (!myCars || myCars.length === 0)
     return (
-      <main className="flex flex-col items-center justify-center gap-20 border border-white p-5 text-3xl text-primary-white">
+      <main className="flex flex-col items-center justify-center gap-20 p-5 text-3xl text-primary-white">
         <h1 className="font-medium">You don&apos;t have any cars</h1>
         <Button
           path={Links.NEW_CAR}
@@ -36,6 +62,7 @@ export default function MyCars() {
     if (!carType) throw new Error('Car type not found')
     return carType
   }
+
   return (
     <main className="px-4">
       <h1 className="my-8 w-full text-center font-lora text-3xl font-medium text-primary-white">
@@ -44,13 +71,17 @@ export default function MyCars() {
       <div className="grid grid-cols-1 gap-4 md:mx-0 md:w-full md:gap-8 md:px-20 lg:grid-cols-2">
         {myCars.map(car => (
           <CarCard key={car.id} car={car} user={user} carType={getCarType(car.carTypeId)}>
-            <Button behavior={ButtonBehavior.Button} customStyles={ButtonStyles.delete}>
+            <Button
+              behavior={ButtonBehavior.Button}
+              onClick={() => setDeleteId(car.id)}
+              customStyles={ButtonStyles.delete}
+            >
               Delete Car
             </Button>
           </CarCard>
         ))}
       </div>
-      <div className="flex md:w-1/2 md:mx-auto py-8">
+      <div className="flex py-8 md:mx-auto md:w-1/2">
         <Button
           path={Links.NEW_CAR}
           behavior={ButtonBehavior.Link}
