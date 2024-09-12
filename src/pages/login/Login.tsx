88c72e088fcc75ef8e71932872ee63cebebcf074
AL2002MI08 <alexandramizero@gmail.com>
2024-09-12 15:04:24 +0200

    fix: fix responsiveness of login form on desktop

--- src/pages/login/Login.tsx
+++ src/pages/login/Login.tsx
@@ -25,11 +25,11 @@ export default function Login() {
   }, [actionData])
 
   return (
-    <main className="mx-4 flex min-h-screen flex-col items-center justify-evenly">
-      <h1 className="flex flex-col text-center font-lora text-5xl font-bold text-gray-50 md:flex-row md:justify-center md:gap-3 lg:text-7xl">
+    <main className="mx-4 flex min-h-screen flex-col items-center justify-evenly md:min-h-fit">
+      <h1 className="flex flex-col text-center font-lora text-5xl font-bold text-gray-50 md:mt-14 md:flex-row md:justify-center md:gap-3 lg:text-7xl">
         CAR <span className="font-medium italic">sharing</span>
       </h1>
-      <div className="flex w-full flex-col items-center gap-8 pt-28">
+      <div className="flex w-full flex-col items-center gap-8 md:pt-20">
         <p className="text-center font-lora text-xl font-medium text-white">Log in</p>
 
         {actionData && <ValidationErrors actionData={actionData} />}
