5baeb35c87b9eb749dea61ad452e752f1bfcfc70
InshutiSouvede <isouvede2@gmail.com>
2024-09-26 12:42:48 +0200

    fix: enable toasts to display when car is deleted or added

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,8 @@ import { ReactElement } from 'react'
 import { configure } from 'axios-hooks'
 import { RouterProvider } from 'react-router-dom'
 import router from './routes/router'
+import { ChakraProvider } from '@chakra-ui/react'
+import customTheme from './chakra/theme'
 
 // Configure axios hooks
 // Do not delete this if you want to use the provided API hooks in `src/hooks`
@@ -11,5 +13,9 @@ configure({
   },
 })
 export default function App(): ReactElement {
-  return <RouterProvider router={router} />
+  return (
+    <ChakraProvider theme={customTheme}>
+      <RouterProvider router={router} />
+    </ChakraProvider>
+  )
 }
--- src/components/DateInput.tsx
+++ src/components/DateInput.tsx
@@ -2,6 +2,7 @@ import dayjs, { Dayjs } from 'dayjs'
 import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs'
 import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider'
 import { StaticDateTimePicker } from '@mui/x-date-pickers/StaticDateTimePicker'
+import { ThemeProvider, createTheme } from '@mui/material/styles'
 import Input, { InputBehavior } from './UI/Input'
 import { useState } from 'react'
 
@@ -31,14 +32,16 @@ export default function DateInput({ placeholder, name }: DateInputProps) {
       />
       {open && (
         <div className="absolute inset-x-4 top-20 max-h-screen overflow-hidden">
-          <LocalizationProvider dateAdapter={AdapterDayjs}>
-            <StaticDateTimePicker
-              defaultValue={dayjs('2022-04-17T15:30')}
-              onChange={handleChange}
-              onClose={() => setOpen(false)}
-              onAccept={() => setOpen(false)}
-            />
-          </LocalizationProvider>
+          <ThemeProvider theme={createTheme()}>
+            <LocalizationProvider dateAdapter={AdapterDayjs}>
+              <StaticDateTimePicker
+                defaultValue={dayjs('2022-04-17T15:30')}
+                onChange={handleChange}
+                onClose={() => setOpen(false)}
+                onAccept={() => setOpen(false)}
+              />
+            </LocalizationProvider>
+          </ThemeProvider>
         </div>
       )}
     </>
