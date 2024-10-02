a51390935d3c1a8b2d1f671e8cabbaf5d7298536
l-isaro <l.isaro@alustudent.com>
2024-10-02 11:00:04 +0200

    fix: refactor desktop responsiveness

--- src/pages/NotFoundPage.tsx
+++ src/pages/NotFoundPage.tsx
@@ -7,15 +7,13 @@ export default function NotFoundPage(): ReactElement {
   return (
     <main className="max-h-screen bg-moni-indigo-800 text-moni-gray-100">
       <div className="flex w-full flex-col px-4 py-10 md:mx-auto md:h-min md:w-1/2 md:py-5 ">
-        <h1 className="flex flex-col text-center font-lora text-5xl font-bold text-moni-gray-100">
-          OOOOOPS!
-        </h1>
+        <h1 className="text-center font-lora text-5xl font-bold text-moni-gray-100">OOOOOPS!</h1>
 
         <div className="flex justify-center">
           <NotFoundIcon />
         </div>
 
-        <p className="mx-auto my-10 text-center font-lora text-2xl font-bold text-moni-gray-100 md:my-16">
+        <p className="mx-auto py-10 text-center font-lora text-2xl font-bold  text-moni-gray-100 md:py-0 md:pb-10">
           Page Not Found
         </p>
 
